87b6750d780a251176741df7fc1d6ccbb93bd3eb
giovannilampis <giolampis@hotmail.com>
2023-09-29 14:48:04 +0200

    add Calculator class

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+
+    public float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public float divide(float num1, float num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return num1 / num2;
+    }
+
+    public float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+}
